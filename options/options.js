ad8c7512bef87a8cc288310e3feddab7a9861f7a
Crowdbotics <team@crowdbotics.com>
2021-09-07 13:12:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "xact_dealer_30373Identifier",
+  url: "https://xact_dealer_30373Identifier.botics.co",
+  api: "https://xact_dealer_30373Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
